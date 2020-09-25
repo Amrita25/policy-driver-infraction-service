afa89c0cf17ec938552ab390f68bd6362ef09dda
Amrita25 <amrita.majumdar25@gmail.com>
2020-09-25 17:16:55 +0530

    first commit

---
+++ src/main/java/io/driverinfractionservice/policydriverinfractionservice/PolicyDriverInfractionServiceApplication.java
@@ -0,0 +1,13 @@
+package io.driverinfractionservice.policydriverinfractionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyDriverInfractionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyDriverInfractionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/driverinfractionservice/policydriverinfractionservice/PolicyDriverInfractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.driverinfractionservice.policydriverinfractionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyDriverInfractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
